513de17d7e3bf6c88c7bc65ef33cf0a8b400220e
BIT-USER <BIT-USER@DESKTOP-R59UCU3>
2018-01-08 18:16:36 +0900

    8번 완성

--- src/com/javaex/prob/Problem08.java
+++ src/com/javaex/prob/Problem08.java
@@ -1,9 +1,44 @@
 package com.javaex.prob;
 
+import java.util.Scanner;
+
 public class Problem08 {
 
 	public static void main(String[] args) {
-		
+		int deposit;
+		int withdraw;
+		int totalMoney = 0;
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+
+			System.out.println("--------------------------");
+			System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
+			System.out.println("--------------------------");
+			System.out.println("선택 : ");
+			int menuNum = sc.nextInt();
+			switch (menuNum) {
+			case 1:
+				System.out.println("예금액 : ");
+				deposit = sc.nextInt();
+				totalMoney += deposit;
+				break;
+			case 2:
+				System.out.println("출금액 : ");
+				withdraw = sc.nextInt();
+				totalMoney -= withdraw;
+				break;
+			case 3:
+				System.out.println("잔액 : "+totalMoney);
+				break;
+			case 4:
+				System.out.println("프로그램 종료");
+				System.exit(0);
+				break;
+			default:
+				System.out.println("다시 입력해 주세요");
+				break;
+			}
+		}
 	}
 
 }
